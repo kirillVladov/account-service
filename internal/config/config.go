5ee2e4233ab2436d363998305b4acb4dbbc4f80f
kirillVladov <kirillovvlad8@gmail.com>
2026-08-14 01:03:24 +0500

    feat: tokens

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 type Config struct {
 	PostgresUrl string `envconfig:"POSTGRES_URL" required:"true"`
-	JwtSecret   string `envconfig:"JWT_SECRET" required:"true"`
 	GRPCPort    int32  `envconfig:"GRPC_PORT" default:"50051"`
 	DebugPort   int32  `envconfig:"DEBUG_PORT" required:"true"`
 }
